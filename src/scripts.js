d40de32cf1a0cc99151696ad813adf2ee980fed9
Cristina Pena <cristinalpena@gmail.com>
2020-02-06 21:41:50 -0700

    Start skeleton and pseudocoding for hydration DOM manipulation

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,19 @@
 window.addEventListener('load', getUserInfo);
 
-let wrapper = document.querySelector('body');
+// need to go through hydration data, create a new hydration obj using each entry, and push all objects to user.hydrationToDate array
 
 function getUserInfo() {
   let userRepository = new UserRepository(userData);
-  let user = new User(userData[0])
+  let user = new User(userData[0]);
+  let userHydrationData = hydrationData.filter(el => el.userID === user.id);
+  let userHydrationDataObjs = userHydrationData.map(el => {
+    return new Hydration(el.userID, el.date, el.numOunces)
+  });
+  user.hydrationToDate.push(userHydrationDataObjs);
+  let dailyHydration = userHydrationDataObjs.find(hydration => hydration.date === "2019/06/22");
+  // let test = dailyHydration.findWeeklyHydration(user, "2019/06/22");
+  // console.log(test);
+
   wrapper.insertAdjacentHTML('beforeend', `
   <header>
     <h1>Welcome, <span>${user.getUsersFirstName()}</span>!</h1>
@@ -24,12 +33,13 @@ function getUserInfo() {
     </section>
     <section class='hydration-widget'>
       <p>OUNCES OF WATER TODAY:</p>
-      <p class='user-data'></p>
+      <p class='user-data'> </p>
       <p>OUNCES OF WATER THIS WEEK:</p>
-      <p class='user-data'></p>
+      <p class='user-data'> </p>
     </section>
   <main>
   `);
 }
 
+let wrapper = document.querySelector('body');
 //
