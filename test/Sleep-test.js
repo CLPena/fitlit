733a5eea350888d6565c1c45bc3479dcc2097a45
Cristina Pena <cristinalpena@gmail.com>
2020-02-10 13:52:52 -0700

    Fix lint errors for Sleep-test.js and correct all tests except final two (added to project board)

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -14,30 +14,29 @@ describe('Sleep', function() {
   let sleep8;
   let sleep9;
   let sleep10;
+  let sleep11;
+  let sleep12;
+  let sleep13;
+  let sleep14;
 
   beforeEach(function() {
     sleep = new Sleep(1, '2019/06/15', 6.1, 2.2);
-
     sleep1 = new Sleep(1, '2019/06/16', 7, 4.7);
-    sleep11 = new Sleep(1, '2019/06/16', 3, 4.7);
-    sleep12 = new Sleep(1, '2019/06/16', 10, 4.7);
-    sleep13 = new Sleep(1, '2019/06/16', 10, 2.7);
-
-    sleep2 = new Sleep(1, '2019/06/17', 10.8, 3);
-    sleep2 = new Sleep(1, '2019/06/17', 10.8, 3);
-    sleep3 = new Sleep(1, '2019/06/18', 4.1, 2.2);
-    sleep4 = new Sleep(1, '2019/06/19', 5.4, 3.6);
-    sleep5 = new Sleep(1, '2019/06/20', 9.6, 2.9);
-    sleep6 = new Sleep(1, '2019/06/21', 6.1, 3.5);
-    sleep7 = new Sleep(1, '2019/06/22', 5.1, 2.2);
-    sleep8 = new Sleep(1, '2019/06/23', 8.1, 1.6);
-    sleep9 = new Sleep(1, '2019/06/24', 8.9, 3.1);
-    sleep10 = new Sleep(1, '2019/06/25', 4.4, 1.2);
+    sleep2 = new Sleep(1, '2019/06/16', 3, 4.7);
+    sleep3 = new Sleep(1, '2019/06/16', 10, 4.7);
+    sleep4 = new Sleep(1, '2019/06/16', 10, 2.7);
+    sleep5 = new Sleep(1, '2019/06/17', 10.8, 3);
+    sleep6 = new Sleep(1, '2019/06/17', 10.8, 3);
+    sleep7 = new Sleep(1, '2019/06/18', 4.1, 2.2);
+    sleep8 = new Sleep(1, '2019/06/19', 5.4, 3.6);
+    sleep9 = new Sleep(1, '2019/06/20', 9.6, 2.9);
+    sleep10 = new Sleep(1, '2019/06/21', 6.1, 3.5);
+    sleep11 = new Sleep(1, '2019/06/22', 5.1, 2.2);
+    sleep12 = new Sleep(1, '2019/06/23', 8.1, 1.6);
+    sleep13 = new Sleep(1, '2019/06/24', 8.9, 3.1);
+    sleep14 = new Sleep(1, '2019/06/25', 4.4, 1.2);
     sleep.log(sleep);
     sleep.log(sleep1);
-    sleep.log(sleep11);
-    sleep.log(sleep12);
-    sleep.log(sleep13);
     sleep.log(sleep2);
     sleep.log(sleep3);
     sleep.log(sleep4);
@@ -47,6 +46,10 @@ describe('Sleep', function() {
     sleep.log(sleep8);
     sleep.log(sleep9);
     sleep.log(sleep10);
+    sleep.log(sleep11);
+    sleep.log(sleep12);
+    sleep.log(sleep13);
+    sleep.log(sleep14);
   });
 
   it('should be a function', function() {
@@ -74,11 +77,7 @@ describe('Sleep', function() {
   });
 
   it('should accept an array of all sleep data to date', function() {
-    expect(sleep.sleepToDate).to.deep.equal([sleep, sleep1, sleep11, sleep12, sleep13, sleep2, sleep3, sleep4, sleep5, sleep6, sleep7, sleep8, sleep9, sleep10]);
-  });
-
-  it.skip('should accept an array of each user\'s avg sleep quality', function() {
-    expect(sleep.usersAvgSleepQuality.length).to.equal(11);
+    expect(sleep.sleepToDate).to.deep.equal([sleep, sleep1, sleep2, sleep3, sleep4, sleep5, sleep6, sleep7, sleep8, sleep9, sleep10, sleep11, sleep12, sleep13, sleep14]);
   });
 
   it('should find the average hours a user sleeps each night', function() {
@@ -109,7 +108,7 @@ describe('Sleep', function() {
     expect(sleep.getSleepQualityTheWeekOf("2019/06/25")).to.deep.equal([3.6, 2.9, 3.5, 2.2, 1.6, 3.1, 1.2]);
   });
 
-  it('should find users who\'s avg sleep quality is greater than 3 for a given week', function() {
+  it('should find users whose avg sleep quality is greater than 3 for a given week', function() {
     expect(sleep.getBestSleepersTheWeekOf("2019/06/25")).to.deep.equal([sleep4, sleep6, sleep9]);
     expect(sleep.getBestSleepersTheWeekOf("2019/06/21")).to.deep.equal([sleep12, sleep4, sleep6]);
   });
